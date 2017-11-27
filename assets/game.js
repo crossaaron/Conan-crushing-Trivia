3ee6b557d5cd99b4564c1fa3a6ed5e092cf687a0
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2017-11-26 19:45:18 -0500

    questions/answers dropping to divs correctly

--- assets/game.js
+++ assets/game.js
@@ -1,40 +1,64 @@
-// stopwatch js as used in class activity
-var counter = 30
-var questionArray = ["Where is my pen? ", "Are you alive? "]
-var answerArray = [["in my pocket", "in my hand"],["no I'm not", "Yes I am"]]
-var questionCounter = 0
 
-window.onload = function() {
-  timerWrapper();
+var timer = 30
+var triviaQuestions = []
+var triviaAnswers = []
+var count = 0
+
+//basic questions //
+var questionArray = {
+  questions: [{
+    prompt: "Where is my Pen?",
+    answers: ["In my pocket", "In my hand",]
+    }, {
+    prompt: "Are you Alive?",
+    answers: ["yes", "no"]
+    }]
 };
 
+//fades out start jumbotron//
+window.onload = function() {
+  $("#start").click(function() {
+    $('#startScreen').fadeOut(500,function(){
+      $('#gameWrapper').fadeIn(500, getQuestion);
+      timerWrapper();
+    });
+  });
+ 
+}
+
+
+//need a way to space out "divs" so they can be formatted
+function getQuestion() {
+  for (var i = 0; i < questionArray.questions.length; i++) {
+  triviaQuestions.push(questionArray.questions[i].prompt);
+  triviaAnswers.push(questionArray.questions[i].answers); 
+  };
+  $("#triviaQuestions" + count).append(triviaQuestions[count]);
+  $("#triviaAnswers" + count).append("<input type='radio' name='answers'/>" + triviaAnswers[count][0]);
+  $("#triviaAnswers" + count).append("<input type='radio' name='answers'/>" + triviaAnswers[count][1]);
+  count++;
+  if (count < 2) {
+    getQuestion();
+  }
+}
+
+//better clock setup than all of stopwatch code// 
 function timerWrapper() {
   theClock = setInterval(thirtySeconds, 1000);
   function thirtySeconds() {
-    if (counter === 0) {
+    if (timer === 0) {
       clearInterval(theClock);
+      $('#submitButton').append("<input type='submit' name='submit' />")
       alert ("TIME'S UP!!");
     }
-    if (counter > 0) {
-      counter--;
+    if (timer > 0) {
+      timer--;
     }
-    $("#timer").html(counter);
+    $("#timer").html(timer);
+    ;
   }
 }
 
-function generateQuestions() {
-  $("#questions").text(questionArray[questionCounter]);
-  $("#answers").text(answerArray[questionCounter][0] + " " + answerArray[questionCounter][1]);
-}
-
-function quesAnswSelector() {
-  if (questionCounter < 7) {
-  questionCounter++;
-  generateQuestions();
-  }
-}
-
-generateQuestions();
 
 
 
